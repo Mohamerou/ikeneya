71cb267d68edf0fdaad331bdf79a589704c1c720
Mohamerou <kmahamadou01@gmail.com>
2022-12-18 20:10:33 +0000

    bug fixes

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Models\MedicalCard;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
