2c68bcf50de52c10d1e264c364ac8cb829c02410
Mohamerou <104529678+Mohamerou@users.noreply.github.com>
2024-10-08 02:52:56 +0000

    V1 refactored

--- routes/api.php
+++ routes/api.php
@@ -34,46 +34,43 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::put('/doctor-update/{id}', [App\Http\Controllers\DoctorController::class, 'update']);
     Route::delete('/doctor-delete/{id}', [App\Http\Controllers\DoctorController::class, 'destroy']);
 
+    // User related routes
+
+    Route::get('/user-show/{id}', [App\Http\Controllers\UserController::class, 'show']);
+    Route::post('/user-create', [App\Http\Controllers\AuthController::class, 'register']);
+    Route::put('/user-update/{id}', [App\Http\Controllers\UserController::class, 'update']);
+    Route::delete('/user-delete/{id}', [App\Http\Controllers\UserController::class, 'destroy']);
+
+        // User notify doctor
+    Route::post('/check-for-notifications', [App\Http\Controllers\NotificationController::class, 'CheckNotification'])->name('notification.check');
+    Route::post('/get-u/ser-notifications', [App\Http\Controllers\NotificationController::class, 'getUs/erNotifications'])->name('notification.all');
+    Route::post('/mark-as-read', [App\Http\Controllers\NotificationController::class, 'markNotifAsread'])->name('notification.read');
 
 
-
-// User related routes
-
-Route::get('/user-show/{id}', [App\Http\Controllers\UserController::class, 'show']);
-Route::post('/user-create', [App\Http\Controllers\AuthController::class, 'register']);
-Route::put('/user-update/{id}', [App\Http\Controllers\UserController::class, 'update']);
-Route::delete('/user-delete/{id}', [App\Http\Controllers\UserController::class, 'destroy']);
-
-    // User notify doctor
-Route::post('/check-for-notifications', [App\Http\Controllers\NotificationController::class, 'CheckNotification'])->name('notification.check');
-Route::post('/get-u/ser-notifications', [App\Http\Controllers\NotificationController::class, 'getUs/erNotifications'])->name('notification.all');
-Route::post('/mark-as-read', [App\Http\Controllers\NotificationController::class, 'markNotifAsread'])->name('notification.read');
+    Route::post('/rdv-request/', [App\Http\Controllers\RdvController::class, 'handleRdvRequest'])->name('rdv.request');
+    Route::post('/rdv-response/', [App\Http\Controllers\RdvController::class, 'handleRdvResponse'])->name('rdv.response');
 
 
-Route::post('/rdv-request/', [App\Http\Controllers\RdvController::class, 'handleRdvRequest'])->name('rdv.request');
-Route::post('/rdv-response/', [App\Http\Controllers\RdvController::class, 'handleRdvResponse'])->name('rdv.response');
+    // Patient related routes
+    Route::get('/patients', [App\Http\Controllers\PatientController::class, 'index']);
+    Route::get('/patient-show/{id}', [App\Http\Controllers\PatientController::class, 'show']);
+    Route::post('/patient-create', [App\Http\Controllers\PatientController::class, 'store']);
+    Route::put('/patient-update/{id}', [App\Http\Controllers\PatientController::class, 'update']);
+    Route::delete('/patient-delete/{id}', [App\Http\Controllers\PatientController::class, 'destroy']);
 
+    // Hospital related routes
+    Route::get('/hospitals', [App\Http\Controllers\HospitalController::class, 'index']);
+    Route::get('/hospital-show/{id}', [App\Http\Controllers\HospitalController::class, 'show']);
+    Route::post('/hospital-create', [App\Http\Controllers\HospitalController::class, 'store']);
+    Route::put('/hospital-update/{id}', [App\Http\Controllers\HospitalController::class, 'update']);
+    Route::delete('/hospital-delete/{id}', [App\Http\Controllers\HospitalController::class, 'destroy']);
 
-// Patient related routes
-Route::get('/patients', [App\Http\Controllers\PatientController::class, 'index']);
-Route::get('/patient-show/{id}', [App\Http\Controllers\PatientController::class, 'show']);
-Route::post('/patient-create', [App\Http\Controllers\PatientController::class, 'store']);
-Route::put('/patient-update/{id}', [App\Http\Controllers\PatientController::class, 'update']);
-Route::delete('/patient-delete/{id}', [App\Http\Controllers\PatientController::class, 'destroy']);
-
-// Hospital related routes
-Route::get('/hospitals', [App\Http\Controllers\HospitalController::class, 'index']);
-Route::get('/hospital-show/{id}', [App\Http\Controllers\HospitalController::class, 'show']);
-Route::post('/hospital-create', [App\Http\Controllers\HospitalController::class, 'store']);
-Route::put('/hospital-update/{id}', [App\Http\Controllers\HospitalController::class, 'update']);
-Route::delete('/hospital-delete/{id}', [App\Http\Controllers\HospitalController::class, 'destroy']);
-
-// Medical data related routes
-Route::get('/medical-datas', [App\Http\Controllers\MedicalCardController::class, 'index']);
-Route::get('/medical-data-show/{id}', [App\Http\Controllers\MedicalCardController::class, 'show']);
-Route::post('/medical-data-create', [App\Http\Controllers\MedicalCardController::class, 'store']);
-Route::put('/medical-data-update/{id}', [App\Http\Controllers\MedicalCardController::class, 'update']);
-Route::delete('/medical-data-delete/{id}', [App\Http\Controllers\MedicalCardController::class, 'destroy']);
+    // Medical data related routes
+    Route::get('/medical-datas', [App\Http\Controllers\MedicalCardController::class, 'index']);
+    Route::get('/medical-data-show/{id}', [App\Http\Controllers\MedicalCardController::class, 'show']);
+    Route::post('/medical-data-create', [App\Http\Controllers\MedicalCardController::class, 'store']);
+    Route::put('/medical-data-update/{id}', [App\Http\Controllers\MedicalCardController::class, 'update']);
+    Route::delete('/medical-data-delete/{id}', [App\Http\Controllers\MedicalCardController::class, 'destroy']);
 
 });
 
